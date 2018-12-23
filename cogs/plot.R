56f39bb58703a3d0418cf50cfae913934ae44d75
nozma <co.panda.fc3s[at]gmail.com>
2018-12-23 21:40:22 +0900

    プロットにエアコン情報を追加

--- cogs/plot.R
+++ cogs/plot.R
@@ -4,9 +4,35 @@ library(dbplyr)
 library(lubridate)
 library(ggplot2)
 library(tidyr)
+library(patchwork)
 
 
 con <- dbConnect(RSQLite::SQLite(), "/home/rito/db/remoData.db")
+
+tbl(con, "remo") %>%
+  select(time, mode, button) %>%
+  rename(
+    `運転モード` = mode,
+    `エアコン` = button
+  ) %>%
+  collect() %>%
+  mutate(
+    time = as_datetime(time),
+    `エアコン` = ifelse(エアコン == "power-off", "切", "入")
+  ) %>%
+  gather(key, value, -time) %>%
+  ggplot(aes(x = time, y = value, col = value)) +
+  geom_line(size = 5) +
+  scale_x_datetime(date_labels = "%m/%d\n%H:%M") +
+  facet_grid(key ~ ., scales = "free_y") +
+  labs(x = "", y = "") +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    legend.position = 'none'
+    )  -> p1
+
 
 tbl(con, "remo") %>%
   select(time, temperature, humidity, illumination) %>%
@@ -18,10 +44,16 @@ tbl(con, "remo") %>%
   collect() %>%
   mutate(time = as_datetime(time)) %>%
   gather(key, value, -time) %>%
-  ggplot(aes(x = time, y = value)) +
+  ggplot(aes(x = time, y = value, col = key)) +
   geom_line() +
+  scale_x_datetime(date_labels = "%m/%d\n%H:%M") +
   facet_grid(key ~ ., scales = "free_y") +
   labs(x = "", y = "") +
-  theme_bw() -> p
+  theme_minimal() +
+  theme(
+    legend.position = "none"
+  ) -> p2
 
-ggsave("/home/rito/image/plot.png", p)
+p <- p1 / p2 + plot_layout(heights = c(2, 3))
+
+ggsave("/home/rito/image/plot.png", p, width = 5, height = 5)
