66f104937306607822245ca213a6944fc8b7adfe
nozma <co.panda.fc3s[at]gmail.com>
2018-12-24 11:46:53 +0900

    エアコンと運転モードのコード修正

--- cogs/plot.R
+++ cogs/plot.R
@@ -9,6 +9,55 @@ library(patchwork)
 
 con <- dbConnect(RSQLite::SQLite(), "/home/rito/db/remoData.db")
 
+tbl(con, "remo") %>% 
+  select(time, button) %>% 
+  collect() %>% 
+  transmute(
+    time = as_datetime(time),
+    start = time,
+    end = lead(start),
+    button = ifelse(button == "power-off", "切", "入")
+  ) %>% 
+  select(-time) %>% 
+  gather(key, value, -button) %>% 
+  arrange(button, value) %>% 
+  mutate(eventID = rep(seq(1, nrow(.)/2), each = 2)) %>% 
+  ggplot() +
+  geom_line(aes(value, button, group=eventID, color=button), size=5) +
+  scale_x_datetime(date_labels = "%m/%d\n%H:%M") +
+  labs(x = "", y = "エアコン") +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    legend.position = 'none'
+  ) -> p1
+
+tbl(con, "remo") %>% 
+  select(time, mode) %>% 
+  collect() %>% 
+  transmute(
+    time = as_datetime(time),
+    start = time,
+    end = lead(start),
+    mode
+  ) %>% 
+  select(-time) %>% 
+  gather(key, value, -mode) %>% 
+  arrange(mode, value) %>% 
+  mutate(eventID = rep(seq(1, nrow(.)/2), each = 2)) %>% 
+  ggplot() +
+  geom_line(aes(value, mode, group=eventID, color=mode), size=5) +
+  scale_x_datetime(date_labels = "%m/%d\n%H:%M") +
+  labs(x = "", y = "運転モード") +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    legend.position = 'none'
+  ) -> p2
+
+
 tbl(con, "remo") %>%
   select(time, mode, button) %>%
   rename(
@@ -31,7 +80,7 @@ tbl(con, "remo") %>%
     axis.text.x = element_blank(),
     axis.ticks.x = element_blank(),
     legend.position = 'none'
-    )  -> p1
+    )  #-> p1
 
 
 tbl(con, "remo") %>%
@@ -48,12 +97,12 @@ tbl(con, "remo") %>%
   geom_line() +
   scale_x_datetime(date_labels = "%m/%d\n%H:%M") +
   facet_grid(key ~ ., scales = "free_y") +
-  labs(x = "", y = "") +
-  theme_minimal() +
+  labs(x = "", y = "センサーデータ") +
+#  theme_minimal() +
   theme(
     legend.position = "none"
-  ) -> p2
+  ) -> p3
 
-p <- p1 / p2 + plot_layout(heights = c(2, 3))
-
+p <- p1 / p2 / p3 + plot_layout(heights = c(.5, .5, 3))
+p
 ggsave("/home/rito/image/plot.png", p, width = 5, height = 5)
